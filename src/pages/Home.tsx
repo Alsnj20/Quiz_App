cf4255b2e2cdc764a17d54a0315ff0f5b6f4a3e1
Alsnj20 <marieljara656@gmail.com>
2024-08-09 01:04:17 -0500

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+
+
+export default function Home(): JSX.Element {
+
+  
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="shadow-lg p-8 rounded-xl text-center">
+      <h1 className="text-8xl">QuizApp!</h1>
+      <p>Test your knowledge</p>
+      <button className="
+      bg-btnQuiz text-3xl text-white font-bold py-2 px-4 rounded-lg pulse
+      ">
+        Start Quiz
+      </button>
     </div>
   )
 }
